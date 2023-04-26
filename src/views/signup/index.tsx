9fd5cf1200bb13fb31bfdce4b995912b63c6caba
MmD <mmdj27634@gmail.com>
2023-04-26 11:51:44 +0430

    add : redirect to signup page

--- src/views/signup/index.tsx
+++ src/views/signup/index.tsx
@@ -1,121 +1,125 @@
+import { useNavigate } from "react-router"
 import Button from "../../components/common/button/button"
 import TextInput from "../../components/common/text-input/text-input"
 import { Email, Password, Phone, User } from "../../svg/icons"
 import { KetabchehLogo } from "../../svg/ketabcheh-logo"
 
 function Signup() {
-  return (
-    <div
-    className="
-    min-h-screen
-    w-full
-    flex
-    flex-col
-    items-center
-    font-[IRANSans]
-    ">
-
+    const navigate = useNavigate()
+    
+    return (
         <div
         className="
-        flex
-        items-center
-        justify-center
-        gap-6
-        mt-4
-        ">
-
-            <p
-            className="
-            text-[31px]
-            font-bold
-            font-[Kalameh]
-            text-primary
-            ">
-                کتابچه
-            </p>
-
-            <KetabchehLogo
-            className="
-            w-[81px]
-            "
-            />
-
-        </div>
-
-        <img
-        src="auth.png"
-        className="
-        w-[60vw]
-        my-[38px]
-        "
-        />
-
-        <form
-        onSubmit={(e) => {
-            e.preventDefault()
-        }}
-        className="
+        min-h-screen
         w-full
-        mt-[auto]
-        rounded-tl-[20px]
-        rounded-tr-[20px]
-        shadow-[0px_-28px_82px_0]
-        shadow-black/5
-        px-3
-        pb-5
+        flex
+        flex-col
+        items-center
+        font-[IRANSans]
         ">
-            <h1
-            className="
-            w-full
-            text-center
-            text-[26px]
-            my-4
-            font-medium
-            ">
-            ثبت نام در کتابچه
-            </h1>
 
             <div
             className="
             flex
-            flex-col
-            gap-3
+            items-center
+            justify-center
+            gap-6
+            mt-4
             ">
-            
-                <TextInput
-                name="name"
-                Icon={User}
-                label="نام و نام خانوادگی"
+
+                <KetabchehLogo
+                className="
+                w-[81px]
+                "
                 />
-                <TextInput
-                name="email"
-                Icon={Email}
-                label="ایمیل"
-                />
-                <TextInput
-                name="phone"
-                Icon={Phone}
-                label="شماره تلفن"
-                />
-                <TextInput
-                name="password"
-                Icon={Password}
-                label="رمز عبور"
-                />
+
+                <p
+                className="
+                text-[31px]
+                font-bold
+                font-[Kalameh]
+                text-primary
+                ">
+                    کتابچه
+                </p>
+
+            </div>
+
+            <img
+            src="auth.png"
+            className="
+            w-[60vw]
+            my-[38px]
+            "
+            />
+
+            <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                navigate("/home")
+            }}
+            className="
+            w-full
+            mt-[auto]
+            rounded-tl-[20px]
+            rounded-tr-[20px]
+            shadow-[0px_-28px_82px_0]
+            shadow-black/5
+            px-3
+            pb-5
+            ">
+                <h1
+                className="
+                w-full
+                text-center
+                text-[26px]
+                my-4
+                font-medium
+                ">
+                ثبت نام در کتابچه
+                </h1>
 
                 <div
                 className="
-                pt-2
+                flex
+                flex-col
+                gap-3
                 ">
-                    <Button>
-                    ثبت نام
-                    </Button>
-                </div>
-            </div>
-        </form>
+                
+                    <TextInput
+                    name="name"
+                    Icon={User}
+                    label="نام و نام خانوادگی"
+                    />
+                    <TextInput
+                    name="email"
+                    Icon={Email}
+                    label="ایمیل"
+                    />
+                    <TextInput
+                    name="phone"
+                    Icon={Phone}
+                    label="شماره تلفن"
+                    />
+                    <TextInput
+                    name="password"
+                    Icon={Password}
+                    label="رمز عبور"
+                    />
 
-    </div>
-  )
+                    <div
+                    className="
+                    pt-2
+                    ">
+                        <Button>
+                        ثبت نام
+                        </Button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    )
 }
 
 export default Signup
